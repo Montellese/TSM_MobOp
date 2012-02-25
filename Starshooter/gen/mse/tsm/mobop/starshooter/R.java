d736f6d65651b3252c21c91fd1bb5c806045cc17
stimpy-ch <stimpy_ch@gmx.net>
2012-02-25 18:28:50 +0100

    Generierung einführender Menudialoge

--- Starshooter/gen/mse/tsm/mobop/starshooter/R.java
+++ Starshooter/gen/mse/tsm/mobop/starshooter/R.java
@@ -13,11 +13,33 @@ public final class R {
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
+    public static final class id {
+        public static final int mainmenu=0x7f050002;
+        public static final int ripp_button=0x7f050005;
+        public static final int ripp_connectionProgress=0x7f050004;
+        public static final int ripp_input=0x7f050003;
+        public static final int subtext=0x7f050001;
+        public static final int text=0x7f050000;
+    }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int item=0x7f030000;
+        public static final int main=0x7f030001;
+        public static final int request_ip_prompt=0x7f030002;
     }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int hello=0x7f040000;
+        public static final int app_name=0x7f040000;
+        public static final int main_menu_about=0x7f040003;
+        public static final int main_menu_connect=0x7f040001;
+        public static final int main_menu_settings=0x7f040002;
+        public static final int prompt4ip_abort=0x7f04000a;
+        public static final int prompt4ip_continue=0x7f040009;
+        public static final int prompt4ip_errInvalid=0x7f04000b;
+        public static final int prompt4ip_hint=0x7f040008;
+        public static final int prompt4ip_title=0x7f040007;
+        public static final int select_masterSlave_master=0x7f040005;
+        public static final int select_masterSlave_slave=0x7f040006;
+        /**   Obsolet <string name="main_menu_exit">Quit</string> 
+         */
+        public static final int select_masterSlave_title=0x7f040004;
     }
 }
